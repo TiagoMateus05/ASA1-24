22d358c61e3bca74e92975b99d9a81281c146cf7
Tiago Mateus <tiagomateus@MacBook-Pro-de-Tiago-2.local>
2024-12-02 19:18:55 +0000

    resolution table has values, cout needs to be implemented

--- projeto.cpp
+++ projeto.cpp
@@ -2,25 +2,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Information.hpp"
+#include <vector>
+#include <math.h>
+
+#include <chrono>
+
+//[res_val, x, val1, val2]
+const int INFO_ARRAY = 4;
+
 
 class Information{
 	private:
 		int _numInteiros;
 		int _tamanhoSequencia;
-		int **_matrizOperacao;
-		int **_matrizResultados;
-		int *_sequencia;
+		std::vector<std::vector<std::vector<std::array<int, INFO_ARRAY>>>> _matrizResultados;
+		std::vector<std::vector<int>> _matrizOperacao;
+		std::vector<int> _sequencia;
 		int _resultado;
 		void criarMatrizOperacao();
-		void destruirMatrizOperacao();
-		void destruirMatrizResultados();
-		void calcularValorCelula();
+		void calcularValorCelulas();
 
 	public:
 		Information(int numInteiros, int tamanhoSequencia);
-		~Information();
-		void printInfo();
+		//void printInfo();
 		void criarMatrizResultados();
 };
 
@@ -28,93 +32,59 @@ Information::Information(int numInteiros, int tamanhoSequencia){
 	_numInteiros = numInteiros;
 	_tamanhoSequencia = tamanhoSequencia;
 	criarMatrizOperacao();
-	_sequencia = new int[_tamanhoSequencia];
+	_sequencia.resize(_tamanhoSequencia);
 	for (int i = 0; i < tamanhoSequencia;i++)
 		std::cin >> _sequencia[i];
 	std::cin >> _resultado;
 	criarMatrizResultados();
 }
 
-Information::~Information(){
-	destruirMatrizOperacao();
-	destruirMatrizResultados();
-	delete[] _sequencia;
-}
-
 void Information::criarMatrizOperacao(){
-	_matrizOperacao = new int*[_numInteiros];
+	_matrizOperacao.resize(_tamanhoSequencia);
 	for (int i = 0; i < _numInteiros; i++){
-		_matrizOperacao[i] = new int[_numInteiros];
+		_matrizOperacao[i].resize(_tamanhoSequencia);
 		for (int a = 0; a < _numInteiros; a++){
 			std::cin >> _matrizOperacao[i][a];
 		}
 	}
 }
 
-void Information::destruirMatrizOperacao(){
-	for (int i = 0; i < _numInteiros; i++){
-		delete[] _matrizOperacao[i];
-	}
-	delete[] _matrizOperacao;
-}
-
-void Information::destruirMatrizResultados(){
-	for (int i = 0; i < _numInteiros; i++){
-		delete[] _matrizResultados[i];
-	}
-	delete[] _matrizResultados;	
-}
-
-void Information::printInfo(){
-
-	std::cout << "Número de inteiros: " << _numInteiros << std::endl;
-	std::cout << "Tamanho da sequência: " << _tamanhoSequencia << std::endl;  
+void Information::calcularValorCelulas(){
 	
-	//Imprime matriz operação
-	for (int i = 0; i < _numInteiros; i++){
-		for (int a = 0; a < _numInteiros; a++){
-			std::cout << _matrizOperacao[i][a] << " ";
+	//[res_val, x, val1, val2]
+	for(int i = 1; i < _tamanhoSequencia; i++){
+		for (int a = 0; (a + i) < _tamanhoSequencia; a++){
+			_matrizResultados[a][a+i].resize(0);
+			//(a, a+i)
+			for (int j = 0; a + j < a + i; j++) {
+				int size_u = _matrizResultados[a][a+j].size();
+				int size_v = _matrizResultados[a+j+1][a+i].size();
+				for(int u = 0; u < size_u; u++ ) {
+					for (int v = 0; v < size_v; v++) {
+						int v1 = _matrizResultados[a][a+j][u][0];
+						int v2 = _matrizResultados[a+j+1][a+i][v][0];
+						_matrizResultados[a][a+i].push_back({_matrizOperacao[v1 - 1][v2 - 1], j, v1, v2});
+					}
+				}
+				
+			}
 		}
-		std::cout << std::endl;
-	}	
-
-	//Imprime Sequência
-	for (int i = 0; i < _tamanhoSequencia; i++)
-		std::cout << _sequencia[i] << " ";
-	std::cout << std::endl;
-
-	//Imprime resultado
-	std::cout << _resultado << std::endl;
-
-	//Imprime matriz resultados
-	std::cout << std::endl << std::endl << "Matriz resultados:" << std::endl;
-	for (int i = 0; i < _tamanhoSequencia; i++){
-		for (int a = 0; a < _tamanhoSequencia; a++){
-			std::cout << _matrizResultados[i][a] << " ";
-		}
-		std::cout << std::endl;
-	}	
-}
-
-void Information::calcularValorCelula(){
-	// Função recursiva que calcula os valores possíveis de cada
-	// célula com base nos outros valores já presentes na tabela.
+	}
 }
 
 void Information::criarMatrizResultados(){
-	_matrizResultados = new int*[_tamanhoSequencia];
+	_matrizResultados.resize(_tamanhoSequencia);
 	for (int i = 0; i < _tamanhoSequencia; i++){
-		_matrizResultados[i] = new int[_tamanhoSequencia];
-		_matrizResultados[i][i] = _sequencia[i];
-		std::cout << "i: " << i << std::endl << "sequencia[i]: " << _sequencia[i] << std::endl;
+		_matrizResultados[i].resize(_tamanhoSequencia);
+		_matrizResultados[i][i].push_back({ _sequencia[i], -1, -1, -1});
 	}
-	for (int i = 0; i < _tamanhoSequencia; i++){
-		for (int a = i + 1; a < _tamanhoSequencia; a++){
-			_matrizResultados[i][a] = 0;
-			calcularValorCelula();
-		}
-	}
+	auto start = std::chrono::high_resolution_clock::now();
+	std::cout << "Start: " << std::endl;
+	calcularValorCelulas();
+	auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
+    std::cout << "Time taken: " << duration.count() << " milliseconds" << std::endl;
 }
 
 int main(){
@@ -131,12 +101,10 @@ int main(){
 	// we want to make sure it outputs.
 	std::cin.tie(0);
 
-
 	int numInteiros;
 	std::cin >> numInteiros;
 	int tamanhoSequencia;
 	std::cin >> tamanhoSequencia;
-
 	Information info (numInteiros, tamanhoSequencia);
-	info.printInfo();
+	return 0;
 }
